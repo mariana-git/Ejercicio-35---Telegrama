9de0c8accadee2b1fb24e49f4ecda3891411aad9
Mariana <marianasayagosistemas@gmail.com>
2021-08-14 08:36:29 -0300

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace Telegrama
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.rtbTexto = new System.Windows.Forms.RichTextBox();
+            this.btnAnalizar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.lbxAnalisis = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Texto del Telegrama:";
+            // 
+            // rtbTexto
+            // 
+            this.rtbTexto.Location = new System.Drawing.Point(34, 58);
+            this.rtbTexto.Name = "rtbTexto";
+            this.rtbTexto.Size = new System.Drawing.Size(288, 96);
+            this.rtbTexto.TabIndex = 1;
+            this.rtbTexto.Text = "";
+            // 
+            // btnAnalizar
+            // 
+            this.btnAnalizar.Location = new System.Drawing.Point(247, 160);
+            this.btnAnalizar.Name = "btnAnalizar";
+            this.btnAnalizar.Size = new System.Drawing.Size(75, 23);
+            this.btnAnalizar.TabIndex = 2;
+            this.btnAnalizar.Text = "Analizar";
+            this.btnAnalizar.UseVisualStyleBackColor = true;
+            this.btnAnalizar.Click += new System.EventHandler(this.BtnAnalizar_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(34, 341);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpiar.TabIndex = 6;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.BtnLimpiar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(247, 341);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 7;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.BtnSalir_Click);
+            // 
+            // lbxAnalisis
+            // 
+            this.lbxAnalisis.BackColor = System.Drawing.SystemColors.Menu;
+            this.lbxAnalisis.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.lbxAnalisis.FormattingEnabled = true;
+            this.lbxAnalisis.ItemHeight = 15;
+            this.lbxAnalisis.Location = new System.Drawing.Point(34, 213);
+            this.lbxAnalisis.Name = "lbxAnalisis";
+            this.lbxAnalisis.Size = new System.Drawing.Size(288, 105);
+            this.lbxAnalisis.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(353, 386);
+            this.Controls.Add(this.lbxAnalisis);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnAnalizar);
+            this.Controls.Add(this.rtbTexto);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Telegrama";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox rtbTexto;
+        private System.Windows.Forms.Button btnAnalizar;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.ListBox lbxAnalisis;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace Telegrama
+{
+    /*Ejercicio 35: Dado un texto de un telegrama que termina en punto:
+        a) Contar la cantidad de palabras que posean más de 10 letras
+        b) Informar la cantidad de veces que aparece cada vocal
+        c) Informar el porcentaje de espacios en blanco.
+        Nota: Las palabras están separadas por un espacio en blanco.*/
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            rtbTexto.Focus();
+            AcceptButton = btnAnalizar;
+        }
+
+        private void BtnAnalizar_Click(object sender, EventArgs e)
+        {
+            string texto = rtbTexto.Text;
+            double porcEspacios;
+            
+            //escribo las expresiones regulares
+            string palDiezLetras = @"\b(\w{10,})\b";
+            string vocalA = @"([aAáÁàÀäÄ])";
+            string vocalE = @"([eEéÉèÈëË])";
+            string vocalI = @"([iIíÍìÌïÏ])";
+            string vocalO = @"([oOóÓöÖòÒ])";
+            string vocalU = @"([uUúÚùÙüÜ])";
+            string espacios = @"(\s)";
+            string noespacios = @"\w";
+
+            //instancio la clase que creé
+            var cant = new UsandoRegex();
+            
+            //instancio los objetos en esa clase a la vez que agrego al listbox el resultado 
+            lbxAnalisis.Items.Add($"Se encontraron {cant.Resultado(palDiezLetras,texto)} palabras de DIEZ o más letras.");
+            lbxAnalisis.Items.Add($"Se encontraron {cant.Resultado(vocalA, texto)} vocales A.");
+            lbxAnalisis.Items.Add($"Se encontraron {cant.Resultado(vocalE, texto)} vocales E.");
+            lbxAnalisis.Items.Add($"Se encontraron {cant.Resultado(vocalI, texto)} vocales I.");
+            lbxAnalisis.Items.Add($"Se encontraron {cant.Resultado(vocalO, texto)} vocales O.");
+            lbxAnalisis.Items.Add($"Se encontraron {cant.Resultado(vocalU, texto)} vocales U.");
+            porcEspacios = (cant.Resultado(espacios, texto)*100)/(cant.Resultado(espacios, texto)+ cant.Resultado(noespacios, texto));
+
+            lbxAnalisis.Items.Add($"El {porcEspacios} % del texto son ESPACIOS.");
+
+            btnAnalizar.Enabled = false;
+
+        }
+        private void BtnLimpiar_Click(object sender, EventArgs e)
+        {
+            rtbTexto.Clear();
+            lbxAnalisis.Items.Clear();
+            btnAnalizar.Enabled = true;
+            rtbTexto.Focus();
+        }
+
+        private void BtnSalir_Click(object sender, EventArgs e)
+        {
+            Dispose();
+        }
+    }
+
+    class UsandoRegex
+    {
+        public double cadena;
+        public double Resultado(String expresion, string texto)
+        {
+            Regex regex = new Regex(expresion);
+            MatchCollection matches = regex.Matches(texto);
+            cadena = matches.Count;
+            return cadena;
+        }
+    }
+}
